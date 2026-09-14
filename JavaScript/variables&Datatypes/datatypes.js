b9af968dcc01b1acc731e3ca04fda853d4bfec50
Aadi Jain <aadij9388@gmail.com>
2026-09-14 13:02:20 +0530

    Add files via upload

---
+++ JavaScript/variables&Datatypes/datatypes.js
@@ -0,0 +1,67 @@
+/*                  Datatypes
+
+1) Primitive= number,string,boolean,undefined,null,bigint,symbol
+
+ number-             takes a memory of 8 bytes 
+ let a=10;
+ let b=2.36;
+ console.log(a,b);
+
+
+string-
+let name="ABC";
+console.log(name);
+
+
+boolean
+let login=true;
+console.log(login);
+
+
+undefined-
+let user;
+console.log(user);         // output will be undefined
+
+const p;
+console.log(p);            // will give error because when any variable with const keyword is written then it must be initialised otherwise it will give error
+
+
+bigint-
+let num=111111111111114666666666666666666666n;
+console.log(num);
+
+
+
+2) Non primitive=
+
+ array
+ let arr=[1,2,3,"ABC",true];
+ console.log(arr);
+
+
+ object-      exists in key value pair
+
+ let user={
+ name:"abc",
+ account:1234,
+ age:"18"
+ }
+
+function-
+
+function print(){
+console.log("Hellooooo");
+}
+
+print();
+
+*/
+
+
+
+
+
+// typeof can be used to know about the datatype
+
+//Primitive datatype are non mutable meaning they can't be changed
+//Non Primitive datatype are mutable meaning they can be changed
---
+++ JavaScript/variables&Datatypes/variables.js
@@ -0,0 +1,35 @@
+// console.log("hello")
+
+//let keyword is used as a prefix for any variable
+
+
+// let name="Abc";
+// let age=20;
+// age=10;
+// console.log(age,name);
+
+
+//const keyword
+// const account=1234;
+
+// account=11111;
+// console.log(account);
+
+
+
+//old method for declaring variable was by using "var" as a prefix but there was a problem with the var keyword,
+//  using it we can declare same variable multiple times 
+
+// var a=10;
+//  var a=20;     //declared two times
+// console.log(a);
+
+
+
+
+
+
+
+
+
+
