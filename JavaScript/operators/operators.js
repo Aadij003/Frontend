cb78754bec94e56d894ffbe2ce95743091aac510
Aadi Jain <aadij9388@gmail.com>
2026-09-19 13:19:17 +0530

    Add files via upload
    Understanding different operators in Javascript.

---
+++ JavaScript/operators/operators.js
@@ -0,0 +1,41 @@
+ // Arithematic 
+// console.log(2+5);
+// console.log(2*5);
+// console.log(2-5);
+// console.log(2/5);
+// console.log(2%5);
+// console.log(2**5);          // 2 will be multiplied 5 times with itself
+
+//Assignment
+// let x=20;
+// let y=30;
+// console.log(x,y);
+// x+=y;
+// console.log(x);
+
+//Comparision
+// let x=10;
+// let y="10";
+// console.log(x>y);
+// console.log(x<y);
+// console.log(x>=y);
+// console.log(x<=y);
+// console.log(x==y);
+
+// console.log(x===y);        // this operator strictly firstly checks whether both numbers have same type, since x is number and y is string so it will return false 
+
+
+// let b=Number(y);      // this will convert y to a number and then assign it to be so therefore type of b would be number
+// let z=String(x);
+
+// let c="abcd";
+// let d=Number(c); 
+// console.log(d);           // output would be NaN i.e. Not a Number and type will be Number
+
+console.log(Number(null));   // null converts to 0 in number
+console.log(Number(undefined)); // output is NaN
+console.log(Boolean(100));    //output = true
+console.log(Boolean(0));      // output= false
+// basically 0 to boolean is false , rest any number except 0 gives true in boolean
+
+// >,<,>=,<= these operators converts ,null--------> number and undefined--------->NaN
