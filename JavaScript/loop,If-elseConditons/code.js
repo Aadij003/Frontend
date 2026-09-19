dcb18967262acdddbc6334974e441319c3efb8b8
Aadi Jain <aadij9388@gmail.com>
2026-09-19 13:20:07 +0530

    Add files via upload
    loop and if else conditions.

---
+++ JavaScript/loop,If-elseConditons/code.js
@@ -0,0 +1,31 @@
+// for loop
+// for(let i=0;i<10;i++){
+//     console.log(i);
+// }
+
+// while loop
+// let i=0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+// do-while loop
+// let i=0;
+// do{
+//     console.log(i);
+//     i++;
+// }
+// while(i<10);
+
+
+//If-else Conditions
+let age=10;
+if(age>=18){
+    console.log("Eligible for voting");
+}
+else{
+    console.log("Not eligible for voting");
+}
+
+
